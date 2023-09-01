efcad2dd3ef0c205944d5cb1c0d3c4f98b61ff6c
Magali18 <pereyrahmagali@gmail.com>
2023-09-01 18:10:07 -0300

    (add/fetch/post)

--- Porftolio_Front_Magali/nombre-de-tu-proyecto/src/components/Home/Home.jsx
+++ Porftolio_Front_Magali/nombre-de-tu-proyecto/src/components/Home/Home.jsx
@@ -1,20 +1,5 @@
 import {useSelector}  from "react-redux";
-
-import  {useDispatch}  from "react-redux";
-import { addVisitor } from "../../redux/visitorSlice";
-import { useEffect } from "react";
-
 const Home = () => {
-  
-  const dispatch = useDispatch();
-  useEffect(() => {
-    fetch("http://localhost:3001/allVisit")
-      .then((res) => res.json())
-      .then((data) => dispatch(addVisitor(data)))
-      .then((data) =>  console.log(data.payload[0].name))
-      .catch((err) => console.log(err));
-  }, [dispatch]);
- 
   const visitor = useSelector((state) => state.visitor);
   return (
     <div>

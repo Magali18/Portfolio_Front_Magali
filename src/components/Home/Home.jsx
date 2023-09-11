0bc6bbae397788ffbb2abd5dad2db58c0a9cdc82
Magali18 <pereyrahmagali@gmail.com>
2023-09-10 23:52:55 -0300

    add/image

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,22 +1,22 @@
 import { useSelector } from "react-redux";
-import"./Home.css"
-import IconTecnologi from "../IconTech/icon"
-import goBack from "../../utils/goBack"
-import NavBar from "../Nav/Nav"
-
-
+import "./Home.css";
+import IconTecnologi from "../IconTech/icon";
+import goBack from "../../utils/goBack";
+import NavBar from "../Nav/Nav";
+import FotoPerfil from '../../image/Screenshot_7.png'
 
 const Home = () => {
   const visitor = useSelector((state) => state.visitor.name);
-  
-  return (
-    <div>
-      <NavBar/>
-<button onClick={goBack}> '' </button>
 
-<h1>BIENVENIDO : {visitor? visitor: 'Usuario N° 23'} </h1>
+  return (
+    <div className="Home">
+<button onClick={goBack}> Atras</button>
+      <div className="component1" > 
+      <img src={FotoPerfil} alt="" />
+      <h1 >Magali Pereyra </h1>
+      </div>
+      <h1 className="component2">Bienvenido : {visitor? visitor:'Usuario N°'}</h1>
     </div>
-  
-  )
+  );
 };
 export default Home;

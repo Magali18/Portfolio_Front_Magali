5b31d2207b7dbce00d49756b81d09f42bb1f459d
Magali18 <pereyrahmagali@gmail.com>
2023-09-17 19:59:24 -0300

    cambios prueba

---
+++ src/views/Educacion/Educacion.jsx
@@ -0,0 +1,10 @@
+import PDF from '../../components/PDF/Pdf'
+const Educacion =()=>{
+    return(
+        <div className='styleEducacion'>
+            
+<PDF></PDF>
+        </div>
+    )
+};
+export default Educacion;

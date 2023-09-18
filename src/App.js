c8f04389e18131fb2d6a301df059a9c57bb8084e
Magali18 <pereyrahmagali@gmail.com>
2023-09-17 23:34:40 -0300

    add/route

--- src/App.js
+++ src/App.js
@@ -1,26 +1,23 @@
 import React from "react";
 import ReactFullpage from "@fullpage/react-fullpage";
-import './index.css';
-import Landing from './views/Landing/Landing'
-import Home from './views/Home/Home'
-import Proyectos from "./components/Proyectos/Proyecto"
-import Educacion from './views/Educacion/Educacion'
+import "./index.css";
+import Landing from "./views/Landing/Landing";
+import Home from "./views/Home/Home";
+import Proyectos from "./components/Proyectos/Proyecto";
+import Educacion from "./views/Educacion/Educacion";
 import Navbar from "./components/Nav/Nav";
-
-const anchors = ["Abaut", "Experiencia", "Skills"];
+import { Route,Routes } from "react-router-dom";
 
 const App = () => {
+  return (
+    <div>
+      <Navbar />
+      <Routes>
+   
+    <Route path="/home" element={ <Home />}> </Route> 
 
-      return (
-        <div>
-              <Navbar></Navbar>
-          <div> </div>
-          <div><h3><Home/></h3></div>
-      
-       
-
-     
-        </div>
-      );
-    }
+      </Routes>
+    </div>
+  );
+};
 export default App;

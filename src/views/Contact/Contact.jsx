66113b8c2278a451705ffe13cd2f6768ffe31428
Magali18 <pereyrahmagali@gmail.com>
2023-09-20 11:07:22 -0300

    add/style/contact

--- src/views/Contact/Contact.jsx
+++ src/views/Contact/Contact.jsx
@@ -1,24 +1,37 @@
-import './Contact.css'
-const Contact=()=>{
-    return (
-        <div className='containerBody'>
-            <div> 
-            <form type='submit'>
-          <p> Contacta conmigo.</p>  
-<div>
-    <input placeholder='Asunto' type='text' />
-    </div>
-    <input placeholder='Escribi tu nombre' type="text" />
-    <div>
-    <input  className='inputMensaje' placeholder='Escribi aqui tu mensaje'/>
-    </div>
-    <button>Enviar</button>
-    <p>Este mensaje será enviado a mi direccion de correo electronico.</p>
-    <p> Si preferis no hacerlo, tenes otras fromas de contactarme a travez de mis redes sociales podes acceder a ellas haciendo <a href='/'> click aqui. </a></p>
-</form>
-
+import "./Contact.css";
+import { BsFillSendCheckFill } from "react-icons/bs";
+const Contact = () => {
+  return (
+    <div className="containerBody">
+      <div className="containerForm3">
+        <p className="styleheader">
+   
+          Contacta conmigo. <BsFillSendCheckFill className="colorIcono" />
+        </p>
+        <form className="contact" type="submit">
+            <label>Asunto
+          <input placeholder="Escribi el asunto" type="text" />
+          </label>
+          <label>Nombre
+          <input placeholder="Escribi tu nombre" type="text" />
+          </label>
+          <label>Mensaje
+          <input placeholder="Escribi tu mensaje" />
+          </label>
+          <button>Enviar</button>
+        </form>
+        
+        <p className="Stylep">Este mensaje será enviado a mi direccion de correo electronico.</p>
+        <div className="stylePfooter"> 
+        <p>
+          Si preferis no hacerlo, tenes otras fromas de contactarme a travez de
+          mis redes sociales podes acceder a ellas haciendo
+          <a href="/"> click aqui. </a>
+          
+        </p>
         </div>
-        </div>
-    )
+      </div>
+    </div>
+  );
 };
 export default Contact;
--- src/views/Landing/Landing.jsx
+++ src/views/Landing/Landing.jsx
@@ -1,34 +1,7 @@
 import "bootstrap/dist/css/bootstrap.min.css";
-import React, { useRef } from "react";
-import emailjs from "@emailjs/browser";
-import { useDispatch } from "react-redux";
 import "./Landing.css";
 
-
-
 const Landing = () => {
-  
-  const form = useRef();
-  //--------------------SEND EMAIL----------------------------
-
-  const sendEmail = (e) => {
-    e.preventDefault();
-    emailjs
-      .sendForm(
-        "service_fwmu3vq",
-        "template_dqufk4h",
-        form.current,
-        "A-0VQcem-nMbwDjM6"
-      )
-      .then(
-        (result) => {
-          console.log(result.text);
-        },
-        (error) => {
-          console.log(error.text);
-        }
-      );
-  };
   return (
     <div>
       <div className="containerForm">

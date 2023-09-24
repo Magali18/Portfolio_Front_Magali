06ef8d08631c3432476c90f1dbfe61d93aefc9d7
Magali18 <pereyrahmagali@gmail.com>
2023-09-24 02:00:36 -0300

    v25/dep

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,5 +1,4 @@
-import './Card.css'
-import {AiFillYoutube} from  "react-icons/ai";
+
 
 
 const products = [
@@ -53,40 +52,4 @@ export default function Example() {
   )
 }
 
-
-{/*
-function Card ({info}) {
-  const {title,description,image, tech,deploy,demo} = info;
-
-  const onClickDemo = () => {
-    window.open(demo, '_blank');
-  }
-  const onClickDeploy =()=>{
-    window.open(deploy, '_blank')
-  }
-
-  return (
-    <div className='bodyCard' > 
-   <div className='card-single'>
- <h1> {title}</h1> 
-    <img src={image} alt="imagen" />
-
-
-  
-   <p>{description}</p>
-   <h6>Tecnologias aplicadas:</h6>
-
-  <h1 className='iconTech'>  {tech} </h1>
-
-   <div >
-<button onClick={onClickDemo}>Ver demo <AiFillYoutube/></button>
- <button onClick={onClickDeploy}>Ver deploy</button> 
-  </div>
-   </div>
-   </div>
-  );
-}
-
-export default Card;
-*/}
 

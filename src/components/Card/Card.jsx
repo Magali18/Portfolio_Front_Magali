7cc4e192037518d8d264edff5bf232892e5f5326
Magali18 <pereyrahmagali@gmail.com>
2023-09-24 15:43:12 -0300

    v27/dep

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,55 +1,37 @@
+import {AiFillYoutube} from  "react-icons/ai";
 
+function Card ({info}) {
+  const {title,description,image, tech,deploy,demo} = info;
 
+  const onClickDemo = () => {
+    window.open(demo, '_blank');
+  }
+  const onClickDeploy =()=>{
+    window.open(deploy, '_blank')
+  }
 
-const products = [
-  {
-    id: 1,
-    name: 'Basic Tee',
-    href: '#',
-    imageSrc: 'https://tailwindui.com/img/ecommerce-images/product-page-01-related-product-01.jpg',
-    imageAlt: "Front of men's Basic Tee in black.",
-    price: '$35',
-    color: 'Black',
-  },
-  // More products...
-]
-
-export default function Example() {
   return (
-    <div className="bg-white" >
-      <div className="mx-auto max-w-2xl px-4 py-8 sm:px-6 sm:py-12 lg:max-w-7xl lg:px-8"
-                 style={{ maxWidth: '100%', maxHeight: '100%' }}>
-        <h2 className="text-lg font-bold tracking-tight text-gray-900">Customers also purchased</h2>
+    <div className='bodyCard' > 
+   <div className='card-single'>
+ <h1> {title}</h1> 
+    <img src={image} alt="imagen" />
 
-        <div className="mt-4 grid grid-cols-1 gap-x-4 gap-y-6 sm:grid-cols-2 lg:grid-cols-3 xl:gap-x-6">
-          {products.map((product) => (
-            <div key={product.id} className="group relative">
-              <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md bg-gray-200 lg:aspect-none group-hover:opacity-75">
-                <img
-                  src={product.imageSrc}
-                  alt={product.imageAlt}
-                  className="h-full w-full object-cover object-center lg:h-40 lg:w-full"
-                  style={{ maxWidth: '100%', maxHeight: '100%' }} 
-                />
-              </div>
-              <div className="mt-2 flex justify-between">
-                <div>
-                  <h3 className="text-sm text-gray-700">
-                    <a href={product.href}>
-                      <span aria-hidden="true" className="absolute inset-0" />
-                      {product.name}
-                    </a>
-                  </h3>
-                  <p className="mt-1 text-xs text-gray-500">{product.color}</p>
-                </div>
-                <p className="text-sm font-medium text-gray-900">{product.price}</p>
-              </div>
-            </div>
-          ))}
-        </div>
-      </div>
-    </div>
-  )
+
+  
+   <p>{description}</p>
+   <h6>Tecnologias aplicadas:</h6>
+
+  <h1 className='iconTech'>  {tech} </h1>
+
+   <div >
+<button onClick={onClickDemo}>Ver demo <AiFillYoutube/></button>
+ <button onClick={onClickDeploy}>Ver deploy</button> 
+  </div>
+   </div>
+   </div>
+  );
 }
+
+export default Card;
 
 
